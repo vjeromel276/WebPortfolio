204c4d75e83a1a1d7e6fafde20be760599c97b99
Vincnet J Lettau II <vjerome2@live.com>
2021-12-28 06:58:40 -0500

    working on ticker

--- short-video-mern/short-video-frontend/src/components/VideoFooter.jsx
+++ short-video-mern/short-video-frontend/src/components/VideoFooter.jsx
@@ -1,7 +1,6 @@
 import "./VideoFooter.css";
 
 import { MusicNote } from "@material-ui/icons";
-import Ticker from "react-ticker";
 
 export const VideoFooter = () => {
   return (
@@ -11,15 +10,6 @@ export const VideoFooter = () => {
         <p>Macbook Air to new Windows editing beast</p>
         <div className="videoFooter__ticker">
           <MusicNote className="videoFooter__icon" />
-          <Ticker>
-            {({ index }) => (
-              <>
-                <h1 style={{ paddingRight: "0.5em" }}>
-                  This is the Headline of element #{index}!
-                </h1>
-              </>
-            )}
-          </Ticker>
         </div>
       </div>
     </div>
