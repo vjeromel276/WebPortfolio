7568701a1cea2b6cbb167855c11d2fb263a37266
Vincnet J Lettau II <vjerome2@live.com>
2021-12-28 15:58:11 -0500

    commit

--- short-video-mern/short-video-frontend/src/App.js
+++ short-video-mern/short-video-frontend/src/App.js
@@ -5,26 +5,7 @@ import { Video } from "./components/Video";
 function App() {
   return (
     <div className="app">
-      <div className="app__videos">
-        <Video
-          url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169738/video1_cvrjfm.mp4"
-          channel="nabendu82"
-          description="Macbook Air to new Windows editing beast"
-          song="I am a Windows PC"
-          likes={345}
-          shares={200}
-          messages={90}
-        />
-        <Video
-          url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169739/video2_mecbdo.mp4"
-          channel="thewebdev"
-          description="Tuesday morning editing on kdenlive in Windows"
-          song="Kdenlive is great"
-          likes={445}
-          shares={290}
-          messages={109}
-        />
-      </div>
+      <Video />
     </div>
   );
 }
