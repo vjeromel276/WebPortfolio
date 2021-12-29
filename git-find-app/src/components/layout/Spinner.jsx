f5f9118ff70595c3367ba9b8269cc3b47b810b6c
Vincnet J Lettau II <vjerome2@live.com>
2021-12-28 22:59:53 -0500

    more class comp's to FCs adding github api key

---
+++ git-find-app/src/components/layout/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from "./spinner.gif";
+
+export const Spinner = () => (
+  <>
+    <img
+      style={{ width: "200px", margin: "auto", display: "block" }}
+      src={spinner}
+      alt="spinner"
+    />
+  </>
+);
--- git-find-app/src/components/users/Users.jsx
+++ git-find-app/src/components/users/Users.jsx
@@ -1,22 +1,30 @@
-import React, { Component } from "react";
-
+import PropTypes from "prop-types";
+import { Spinner } from "../layout/Spinner";
 import { UserItem } from "./UserItem";
 
-export class Users extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div style={userStyle}>
-          {this.props.users.map((user) => (
+export const Users = ({ users, loading }) => {
+  console.log(loading);
+  return (
+    <div className="container">
+      <div style={userStyle}>
+        {users.map((user) =>
+          !loading ? (
             <UserItem key={user.id} user={user} />
-          ))}
-        </div>
+          ) : (
+            <Spinner key={user.id} />
+          )
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3,1fr)",
   gridGap: "1rem",
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
