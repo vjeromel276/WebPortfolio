6a8e6ea5d2aa8a113e727bde6d668246889213cb
Vincnet J Lettau II <vjerome2@live.com>
2022-01-06 02:20:58 -0500

    connected to mongodb

--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//^  Connect MongoDB  ^\\
+connectDB();
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the ContactKeeper API..." })
