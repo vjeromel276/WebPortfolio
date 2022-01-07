4acf250fc2652212aba8aeddfe28ca9b5287ce90
Vincnet J Lettau II <vjerome2@live.com>
2022-01-07 05:49:55 -0500

    after user model and validation

--- contact-keeper/routes/users.js
+++ contact-keeper/routes/users.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 //&  @route POST api/users
 //&  @desc Register a user
 //&  @access Public
 
-router.post("/", (req, res) => {
-  res.send("Registers a user");
-  console.log("Registers a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please enter a name").notEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("Passed");
+  }
+);
 
 module.exports = router;
